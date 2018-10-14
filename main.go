f3f5ee50307aff60c97e1aaf71fe23c3c2e86fa9
duke.du <duke@MacBook-Pro-2.local>
2018-10-14 16:30:39 +0800

    02_实现SetHash

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"crypto/sha256"
+)
 
 //0. 定义结构
 type Block struct {
@@ -16,14 +19,26 @@ type Block struct {
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := Block{
 		PrevHash: prevBlockHash,
-		Hash:     []byte{}, //先填空，后面再计算 //TODO
+		Hash:     []byte{},
 		Data:     []byte(data),
 	}
+
+	block.SetHash()
 
 	return &block
 }
 
 //3. 生成哈希
+func (block *Block) SetHash() {
+	//1. 拼装数据
+	blockInfo := append(block.PrevHash, block.Data...)
+
+	//2. sha256
+	//func Sum256(data []byte) [Size]byte {
+	hash := sha256.Sum256(blockInfo)
+	block.Hash = hash[:]
+}
+
 //4. 引入区块链
 //5. 添加区块
 //6. 重构代码
@@ -34,5 +49,5 @@ func main() {
 	fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
 	fmt.Printf("当前区块哈希值： %x\n", block.Hash)
 	fmt.Printf("区块数据 :%s\n", block.Data)
-	fmt.Println("hello")
+	//fmt.Println("hello")
 }
