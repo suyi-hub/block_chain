112beaed3a39b34e8b21b723a322005fca3af886
duke.du <duke@MacBook-Pro-2.local>
2018-10-14 17:20:56 +0800

    06_添加完整的区块字段并引用

--- block.go
+++ block.go
@@ -2,24 +2,52 @@ package main
 
 import (
 	"crypto/sha256"
+	"time"
 )
 
 //0. 定义结构
 type Block struct {
-	//1. 前区块哈希
+	//1.版本号
+	Version uint64
+	//2. 前区块哈希
 	PrevHash []byte
-	//2. 当前区块哈希
+	//3. Merkel根（梅克尔根，这就是一个哈希值，我们先不管，我们后面v4再介绍）
+	MerkelRoot []byte
+	//4. 时间戳
+	TimeStamp uint64
+	//5. 难度值
+	Difficulty uint64
+	//6. 随机数，也就是挖矿要找的数据
+	Nonce uint64
+
+	//a. 当前区块哈希,正常比特币区块中没有当前区块的哈希，我们为了是方便做了简化！
 	Hash []byte
-	//3. 数据
+	//b. 数据
 	Data []byte
+}
+
+//1. 补充区块字段
+//2. 更新计算哈希函数
+//3. 优化代码
+
+//实现一个辅助函数，功能是将uint64转成[]byte
+
+func Uint64ToByte(num uint64) []byte {
+	//TODO
+	return []byte{}
 }
 
 //2. 创建区块
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := Block{
-		PrevHash: prevBlockHash,
-		Hash:     []byte{},
-		Data:     []byte(data),
+		Version:    00,
+		PrevHash:   prevBlockHash,
+		MerkelRoot: []byte{},
+		TimeStamp:  uint64(time.Now().Unix()),
+		Difficulty: 0, //随便填写的无效值
+		Nonce:      0, //同上
+		Hash:       []byte{},
+		Data:       []byte(data),
 	}
 
 	block.SetHash()
@@ -29,8 +57,15 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 
 //3. 生成哈希
 func (block *Block) SetHash() {
+	var blockInfo []byte
 	//1. 拼装数据
-	blockInfo := append(block.PrevHash, block.Data...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Version)...)
+	blockInfo = append(blockInfo, block.PrevHash...)
+	blockInfo = append(blockInfo, block.MerkelRoot...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.TimeStamp)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Difficulty)...)
+	blockInfo = append(blockInfo, Uint64ToByte(block.Nonce)...)
+	blockInfo = append(blockInfo, block.Data...)
 
 	//2. sha256
 	//func Sum256(data []byte) [Size]byte {
