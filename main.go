b306bdb6373808e130bb54feba08ced2db08ca2e
duke.du <duke@MacBook-Pro-2.local>
2018-10-15 17:51:42 +0800

    14_实现命令命令功能,v3大结局

--- main.go
+++ main.go
@@ -10,21 +10,4 @@ func main() {
 	//bc.AddBlock("111111111111111")
 	//bc.AddBlock("222222222222222")
 	//
-	////创建迭代器
-	//it := bc.NewIterator()
-	//
-	////调用迭代器，返回我们的每一个区块数据
-	//for {
-	//	//返回区块，左移
-	//	block := it.Next()
-	//
-	//	fmt.Printf("===========================\n\n")
-	//	fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
-	//	fmt.Printf("当前区块哈希值： %x\n", block.Hash)
-	//	fmt.Printf("区块数据 :%s\n", block.Data)
-	//
-	//	if len(block.PrevHash) == 0 {
-	//		fmt.Printf("区块链遍历结束！")
-	//		break
-	//	}
-	//}
+
