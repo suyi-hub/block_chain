89300ebf21ecf674afe77d968838945abec198fd
duke.du <duke@MacBook-Pro-2.local>
2018-10-15 17:31:01 +0800

    13_添加命令调用（功能尚未实现）

--- main.go
+++ main.go
@@ -1,28 +1,30 @@
 package main
 
-import "fmt"
+//import "fmt"
 
 func main() {
 	bc := NewBlockChain()
-	bc.AddBlock("111111111111111")
-	bc.AddBlock("222222222222222")
-
-	//创建迭代器
-	it := bc.NewIterator()
-
-	//调用迭代器，返回我们的每一个区块数据
-	for {
-		//返回区块，左移
-		block := it.Next()
-
-		fmt.Printf("===========================\n\n")
-		fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
-		fmt.Printf("当前区块哈希值： %x\n", block.Hash)
-		fmt.Printf("区块数据 :%s\n", block.Data)
-
-		if len(block.PrevHash) == 0 {
-			fmt.Printf("区块链遍历结束！")
-			break
-		}
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
+	//bc.AddBlock("111111111111111")
+	//bc.AddBlock("222222222222222")
+	//
+	////创建迭代器
+	//it := bc.NewIterator()
+	//
+	////调用迭代器，返回我们的每一个区块数据
+	//for {
+	//	//返回区块，左移
+	//	block := it.Next()
+	//
+	//	fmt.Printf("===========================\n\n")
+	//	fmt.Printf("前区块哈希值： %x\n", block.PrevHash)
+	//	fmt.Printf("当前区块哈希值： %x\n", block.Hash)
+	//	fmt.Printf("区块数据 :%s\n", block.Data)
+	//
+	//	if len(block.PrevHash) == 0 {
+	//		fmt.Printf("区块链遍历结束！")
+	//		break
+	//	}
+	//}
