3e76069da3fa7224b67969da4c60a5ef0a097c22
duke.du <duke@MacBook-Pro-2.local>
2018-10-15 11:57:33 +0800

    bolt数据库demo读操作

--- demo/boltTest.go
+++ demo/boltTest.go
@@ -6,12 +6,11 @@ import (
 	"log"
 )
 
-func main()  {
-	fmt.Println("helloworld")
-
-
+func main() {
 	//1. 打开数据库
 	db, err := bolt.Open("test.db", 0600, nil)
+	defer db.Close()
+
 	if err != nil {
 		log.Panic("打开数据库失败！")
 	}
@@ -28,13 +27,29 @@ func main()  {
 			}
 		}
 
+		//3. 写数据
 		bucket.Put([]byte("11111"), []byte("hello"))
 		bucket.Put([]byte("22222"), []byte("world"))
 
 		return nil
 	})
 
-
-	//3. 写数据
 	//4. 读数据
+	db.View(func(tx *bolt.Tx) error {
+		//1. 找到抽屉，没有的化直接报错退出
+		bucket := tx.Bucket([]byte("b1"))
+		if bucket == nil {
+			log.Panic("bucket b1 不应该为空，请检查!!!!")
+		}
+
+		//2. 直接读取数据
+		v1 := bucket.Get([]byte("11111"))
+		v2 := bucket.Get([]byte("22222"))
+
+		fmt.Printf("v1 : %s\n", v1)
+		fmt.Printf("v2 : %s\n", v2)
+
+		return nil
+	})
+
 }
