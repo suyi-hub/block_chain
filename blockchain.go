c363079431dc64eab03b7346d4733c3cd274856e
duke.du <duke@MacBook-Pro-2.local>
2018-10-18 10:44:37 +0800

    10_v4_实现FindNeedUTXOs函数，尚未测试

--- blockchain.go
+++ blockchain.go
@@ -219,13 +219,3 @@ func (bc *BlockChain) FindUTXOs(address string) []TXOutput {
 
 	return UTXO
 }
-
-func (bc *BlockChain) FindNeedUTXOs(from string, amount float64) (map[string][]uint64, float64) {
-	//找到的合理的utxos集合
-	var utxos map[string][]uint64
-	//找到的utxos里面包含前的总数
-	var calc float64
-
-	//TODO
-	return utxos, calc
-}
