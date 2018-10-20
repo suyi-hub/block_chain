7b18b0390e56882a03718fde331ca0b12e3b3e8c
duke.du <duke@MacBook-Pro-2.local>
2018-10-20 15:20:49 +0800

    06_Sign逻辑框架搭建

--- transaction.go
+++ transaction.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"crypto/sha256"
 	"fmt"
+	"crypto/ecdsa"
 )
 
 const reward = 50
@@ -140,7 +141,7 @@ func NewTransaction(from, to string, amount float64, bc *BlockChain) *Transactio
 
 	//3. 得到对应的公钥，私钥
 	pubKey := wallet.PubKey
-	//privateKey := wallet.Private  //稍后再用
+	privateKey := wallet.Private  //稍后再用
 
 	//传递公钥的哈希，而不是传递地址
 	pubKeyHash := HashPubKey(pubKey)
@@ -179,5 +180,26 @@ func NewTransaction(from, to string, amount float64, bc *BlockChain) *Transactio
 
 	tx := Transaction{[]byte{}, inputs, outputs}
 	tx.SetHash()
+
+
+
+	//签名，交易创建的最后进行签名
+	prevTXs := make(map[string]Transaction)
+
+
+
+
+	tx.Sign(*privateKey, prevTXs)
+
+
 	return &tx
+}
+
+//签名的具体实现,
+// 参数为：私钥，inputs里面所有引用的交易的结构map[string]Transaction
+//map[2222]Transaction222
+//map[3333]Transaction333
+func (tx *Transaction) Sign(privateKey ecdsa.PrivateKey, prevTXs map[string]Transaction) {
+	//具体签名的动作先不管，稍后继续
+	//TODO
 }
